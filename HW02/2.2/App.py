192d4ad063651fdf8b09aabbf3ebd34a7d7f8a2f
kasemsanm <32940983+Kasemsanm@users.noreply.github.com>
2018-02-02 00:41:06 +0700

    send hw2

--- HW02/2.2/App.py
+++ HW02/2.2/App.py
@@ -1,24 +1,27 @@
-from flask import Flask , request ,jsonify
-from flask_restful import Resource , Api,reqparse
-import json ,time 
+from flask import Flask , request ,jsonify,render_template
+import os
+app = Flask(__name__)
 
-app = Flask (__name__)
-api = Api(app) 
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+@app.route("/")
+def index():
+    return render_template("upload.html")
 
-parser = reqparse.RequestParser()
-parser.add_argument('file')
-class Upload(Resource):
-    def post(self, fname):
-        file = request.files['file']
-        if file:
-            pass
-        else:
-            return {'False'}
+@app.route("/upload", methods=['POST'])
+def upload():
+    target = os.path.join(APP_ROOT, 'images/')
+    print(target)
 
-class index(Resource):
-	def get(self):
-		return '<input type="file" name="file">'
-Api.add_resource(Upload, '/upload/<string:fname>')
-Api.add_resource(index,'/')
+    if not os.path.isdir(target):
+        os.mkdir(target)
+
+    for file in request.files.getlist("file"):
+        print(file)
+        filename = file.filename
+        destination = "/".join([target, filename])
+        print(destination)
+        file.save(destination)
+	return jsonify(code=200,desc="Upload success")
+
 if __name__ == '__main__':
 	app.run(host='0.0.0.0',port=5000)
