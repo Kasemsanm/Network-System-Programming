930c7b58cca488ff8c400f68da56fc4af21c7091
Kasemsan Maotha <32940983+Kasemsanm@users.noreply.github.com>
2018-02-02 09:36:31 +0000

    Update Code Lab01

--- Lab01/CallService.cs
+++ Lab01/CallService.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class CallService : MonoBehaviour {
   // Use this for initialization
   void Start () {
+      
+  }
+  // https://jsonplaceholder.typicode.com/users
+  void GetRequest () {
+    string url = "https://jsonplaceholder.typicode.com/users"   
+  }
   
+  IEnumerator WaitForRequest() {
+    
   }
 }
