e0a16267bd611ddd5af493dbe98ee6abeba63b24
Kasemsan Maotha <32940983+Kasemsanm@users.noreply.github.com>
2018-02-09 09:33:26 +0000

    update Lab02

--- Lab02/service.py
+++ Lab02/service.py
@@ -2,10 +2,22 @@ import pymongo
 from flash import Flask,request
 from flash_restful import Resource , Api,reqparse
 
-url = "mongodb://[usermame]:[password]@[host]:[port]"
+url = "mongodb://munu:test1234@host:27017/admin"
 client = pymongo.MongoClient(url)
 
 app = Flask(__name__)
 api = Api(app)
 parser = reqparse.RequestParser()
+parser.add_argument('information')
 
+class Registration(Resource):
+  def post(self):
+    args = parser.parse_args()
+    data = args['information']
+    print(data)
+    return {"data":data}
+
+api.add_resource(Registeration,'/api/regis')
+
+if __name__ == '__main__': 
+  app.run(host='0.0.0.0',port=5000)
